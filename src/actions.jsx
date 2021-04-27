a60956764cea6108c26ac80d1c2ae394e7df6cc3
HsinJouHung <45786914+HsinJouHung@users.noreply.github.com>
2021-04-27 19:24:50 +0800

    Updates

--- src/actions.jsx
+++
@@ -1,8 +0,0 @@
-import * as types from './types'
-
-export function setName(name) {
-    return {
-        type: types.SET_NAME,
-        payload: name
-    }
-}
--- static/js/main.46fdbbcf.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpharvasia-web"]=this["webpackJsonpharvasia-web"]||[]).push([[0],{11:function(n,t,e){"use strict";e.r(t);var a=e(1),i=e.n(a),c=e(3),o=e.n(c),r=(e(8),e.p,e(9),e(0));var s=function(n){n&&n instanceof Function&&e.e(3).then(e.bind(null,12)).then((function(t){var e=t.getCLS,a=t.getFID,i=t.getFCP,c=t.getLCP,o=t.getTTFB;e(n),a(n),i(n),c(n),o(n)}))};o.a.render(Object(r.jsx)(i.a.StrictMode,{}),document.getElementById("root")),s()},8:function(n,t,e){},9:function(n,t,e){}},[[11,1,2]]]);
-//# sourceMappingURL=main.46fdbbcf.chunk.js.map
