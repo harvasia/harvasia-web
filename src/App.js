705a6c17c40120505c50443fa653a08248d3178e
HsinJouHung <45786914+HsinJouHung@users.noreply.github.com>
2021-05-04 19:56:14 +0800

    Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Com from './components/Com'
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <NavBar />
+      <Switch>
+          <Route path="/home" exact component={() => <Com />} />
+        </Switch>
+        </Router>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,8 +9,8 @@ const NavBar = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link href="/home">Home</Nav.Link>
+                    <Nav.Link href="/calendar">Calendar</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
