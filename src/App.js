568e3c2468962e2efa5e940029ead23fa21715a3
HsinJouHung <45786914+HsinJouHung@users.noreply.github.com>
2021-05-04 19:31:57 +0800

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Com from './components/Com'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <Com />
+      <NavBar />
     </div>
   );
 }
